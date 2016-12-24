24eb26d1a7f305921aab83eaa01fbc449ae5d1f8
Nuno Machado <nmiguelmoura@gmail.com>
2016-12-24 11:15:00 +0000

    Add database setup

---
+++ www/database_setup.py
@@ -0,0 +1,31 @@
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+Base = declarative_base()
+
+
+class Restaurant(Base):
+
+    __tablename__ = "restaurant"
+
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+
+class Menu(Base):
+
+    __tablename__ = "menu"
+
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+    course = Column(String(30))
+    description = Column(String(250))
+    price = Column(String(10))
+    restaurant_id = Column(Integer, ForeignKey("restaurant.id"))
+    restaurant = relationship(Restaurant)
+
+
+engine = create_engine('sqlite:///restaurant.db')
+Base.metadata.create_all(engine)
