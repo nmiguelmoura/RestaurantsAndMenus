8a9f39fe5ef1e9c746494a45be49a3aad414a1b3
Nuno Machado <nmiguelmoura@gmail.com>
2017-01-10 18:26:40 +0000

    Display menu images

--- www/menus.py
+++ www/menus.py
@@ -1,13 +1,15 @@
 from flask import render_template, redirect, flash
 import database_interaction
+import os.path
+import prefabs.file_extension_check
+
 
 class Menus:
-
     db_rest = database_interaction.DB_interaction()
+    extension_check = prefabs.file_extension_check.File_extension_check()
 
     def __init__(self):
         pass
-
 
     def launch(self, rest_id):
         restaurant = self.db_rest.query_restaurant_by_id(rest_id)
@@ -15,11 +17,34 @@ class Menus:
         if restaurant:
             menus = self.db_rest.query_menus(rest_id)
             creator = self.db_rest.get_user_info(restaurant.user_id)
+            menu_pictures = {}
 
             if menus:
-                return render_template("menu.html", restaurant=restaurant, menus=menus, creator=creator)
+                extensions = self.extension_check.get_available_extensios()
+                path = ''
+
+                for m in menus:
+                    for ext in extensions:
+                        path = 'uploads/image_menu_%s.%s' % (m.id, ext)
+                        if os.path.isfile('static/%s' % path):
+                            print path
+                            menu_pictures['m_%s' % m.id] = path
+                            continue
+
+                print menu_pictures
+
+                return render_template("menu.html",
+                                       restaurant=restaurant,
+                                       menus=menus,
+                                       creator=creator,
+                                       menu_pictures=menu_pictures)
             else:
-                return render_template("menu.html", restaurant=restaurant, menus=[], creator=creator, no_menus=True)
+                return render_template("menu.html",
+                                       restaurant=restaurant,
+                                       menus=[],
+                                       menu_pictures=[],
+                                       creator=creator,
+                                       no_menus=True)
         else:
             flash("The restaurant you are trying to see does not exist!")
             return redirect("/pagenotfound")
--- www/prefabs/file_extension_check.py
+++ www/prefabs/file_extension_check.py
@@ -1,10 +1,15 @@
 class File_extension_check:
+    ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
     def __init__(self):
         pass
 
     def check(self, filename):
-        ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
-        return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+        return '.' in filename and filename.rsplit('.', 1)[1].lower() in self.ALLOWED_EXTENSIONS
 
     def get_extension(self, filename):
         return '.' in filename and filename.rsplit('.', 1)[1].lower()
+
+    def get_available_extensios(self):
+        return self.ALLOWED_EXTENSIONS
